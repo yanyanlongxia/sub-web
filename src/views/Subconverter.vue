542e8669b0270fd14fcf89d1391e252de0fe4105
yanyanlongxia <59304287+yanyanlongxia@users.noreply.github.com>
2020-10-16 20:29:39 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -265,7 +265,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-	  "sub.yylx.tech(本站提供-稳定)": "https://sub.yylx.tech/sub?",
+          "sub.yylx.tech(本站提供-稳定)": "https://sub.yylx.tech/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)": "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
           "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
